c0bed4d69410f0d99d51db63550cbb48a060c234
hfter789 <hfter789@yahoo-inc.com>
2016-03-15 18:19:29 -0700

    fix typo

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', function(req, res) {
     });
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = app.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });

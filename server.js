c3fdab2eface8ebe78fa257ea03a5f84ff917eb9
hfter789 <hfter789@yahoo-inc.com>
2016-03-15 18:15:45 -0700

    add env port

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('*', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
-    console.log('listening on port 3000!');
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
